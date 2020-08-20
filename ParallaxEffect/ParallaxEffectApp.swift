26696373670c144793116aeb96df5760e0ac0858
Alexander Römer <alex@roemer.design>
2020-08-20 11:57:26 +0200

    Initial Commit

---
+++ ParallaxEffect/ParallaxEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxEffectApp.swift
+//  ParallaxEffect
+//
+//  Created by Alexander Römer on 20.08.20.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
